04f1545924a5fe7290e33ab8ce5002abca2e4fff
thakur-rishabh <rishabhprasadthakur@gmail.com>
2023-09-23 02:27:03 -0500

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,357 @@
+export const Bio = {
+  name: "Rishabh Thakur",
+  roles: [
+    "MLOps Engineer",
+    "Cloud Engineer",
+    "Data Engineer",
+    "Site Reliability Engineer",
+  ],
+  description:
+    " I am a devoted technophile and innovative problem solver with a special passion for optimizing data systems. My journey is fueled by a desire to make information more reliable, accessible, and secure, and this platform is a showcase of my endeavors and insights in the technological realm.",
+  github: "https://github.com/thakur-rishabh",
+  resume:
+    "https://drive.google.com/file/d/18x-FyfbX2GHvB1NAAs4yXLEKXVhURdtQ/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rishabhpthakur/",
+};
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/java/java-original.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bash",
+        image:
+          "https://raw.githubusercontent.com/kerol2r20/Windows-terminal-context-menu/master/icons/git-bash.ico",
+      }
+    ],
+  },
+  {
+    title: "Devops",
+    skills: [
+      {
+        name: "Git",
+        image: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/git/git-original.svg",
+      },
+      {
+        name: "Bamboo CI/CD",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/bamboo/bamboo-original.svg",
+      },
+      {
+        name: "Jenkins",
+        image: "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/jenkins/jenkins-original.svg",
+      },
+      {
+        name: "Ansible",
+        image:
+          "https://raw.githubusercontent.com/ansible/logos/main/vscode-ansible-logo/vscode-ansible.svg",
+      },
+      {
+        name: "GitLab",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/gitlab/gitlab-original.svg",
+      },
+      {
+        name: "GitAction",
+        image: "https://github.githubassets.com/images/modules/site/features/actions-icon-actions.svg",
+      },
+      {
+        name: "CloudFormation",
+        image:
+          "https://raw.githubusercontent.com/weibeld/aws-icons-svg/5e0e14e5472f1eefed879d7ea7e1d79652858d14/q1-2022/Architecture-Service-Icons_01312022/Arch_Management-Governance/32/Arch_AWS-CloudFormation_32.svg",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/docker/docker-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Data Engineer",
+    skills: [
+      {
+        name: "Apache Airflow",
+        image:
+          "https://raw.githubusercontent.com/simple-icons/simple-icons/25e8f1e767d6d3b85d9975e05bb515a2b5cf1f8c/icons/apacheairflow.svg",
+      },
+      {
+        name: "GCP BigQuery",
+        image: "",
+      },
+      {
+        name: "PostgreSQL",
+        image:
+        "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/postgresql/postgresql-original.svg",
+      },
+      {
+        name: "AWS S3",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "AWS Redshift",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Site Reliablity Engineer",
+    skills: [
+      {
+        name: "Grafana",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/55609aa5bd817ff167afce0d965585c92040787a/icons/postgresql/postgresql-original.svg",
+      },
+      {
+        name: "ELK Stack",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Terraform",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "Nagios",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "DataDog",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Problem Solving",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "Communication",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+  {
+    title: "Cloud Platform",
+    skills: [
+      {
+        name: "Amazon Web Service (AWS)",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "Openstack",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Google Cloud Platform (GCP)",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+    ],
+  },
+  {
+    title: "Other",
+    skills: [
+      {
+        name: "VSCode",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Pandas",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "NLP",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D560BAQFDUKPSU8P0Lw/company-logo_200_200/0/1687947132305?e=1698278400&v=beta&t=cBF3SdszRLspmyvV-rHa1y4tdj_hjjivH7Xy0Z7kOD8",
+    role: "Cloud Engineer Intern",
+    company: "Bayer R&D Services LLC",
+    date: "Aug 2023 - Oct 2023",
+    desc: "I served on the MLOps team, achieving a notable 30% reduction in expenses through the implementation of automated AWS Sagemaker systems and CloudFormation, and streamlined operations, saving 10% of time and enhancing data security, by integrating Azure AD & Github OAuth with Airflow2.",
+    skills: [
+      "AWS Sagemaker Studio",
+      "Apache Airflow",
+      "OAuth login",
+      "Cloudformation",
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Python",
+      "Grafana",
+      "DataDog"
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQFQb9_Lb3bZug/company-logo_200_200/0/1644414857251?e=1693440000&v=beta&t=7WqhIiUCg_0otFDwWn-O_SP-Z4Ftoe4be1WlnjYoESc",
+    role: "Student Assistant - IO Security Operation",
+    company: "University of Texas at Arlington",
+    date: "March 2022 - May 2023",
+    desc: "I optimized ticket handling and reduced volume by implementing efficient procedures, maintaining information security, and resolving application issues. I also automated processes to ensure service continuity, achieving a 15% decrease in tickets and a 30% reduction in manual workload through strategic incident management and effective use of Splunk queries for optimized data retrieval.",
+    skills: [
+      "Python",
+      "Splunk",
+      "ITIL"
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C510BAQGSObrO0QPlMQ/company-logo_200_200/0/1555704980721?e=1692835200&v=beta&t=FjOWcUVVNzqFbTmmp0iXQkv5keBQC5lxIQ7K6rmMSMQ",
+    role: "Research Assistant",
+    company: "HBE Lab, University of Texas, Arlington",
+    date: "Aug 2022 - Dec 2023",
+    desc: "Conducted assessment of human-building interactions through utilization of IoT-based non-intrusive monitoring techniques to enhance human comfort and energy efficiency. Utilized diverse sensing techniques and user input to enhance accuracy in monitoring occupant behavior patterns.",
+    skills: [
+      "NLTK",
+      "Raspberry Pi",
+      "SCD40",
+      "Plotly",
+      "Matplot",
+      "Pandas",
+      "Statistics"
+    ],
+    doc: "https://hubilab.uta.edu/",
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/D4D0BAQGfGQHEJt8gPA/company-logo_100_100/0/1666002815185?e=1692230400&v=beta&t=c4obZda4Uh9q6vOcmCbxjoSiExvbvYwEHUlgdY0tU-4",
+    role: "Site Reliability Engineer",
+    company: "Volkswagen Group Technology Solutions",
+    date: "Aug 2018 - Jan 2022",
+    desc: "As a Site Reliability Engineer, I managed infrastructures for nine AEM-based applications, enhancing system reliability through innovative solutions like ELK log monitor stack and fault monitoring metrics. I significantly improved operational efficiency by designing advanced monitoring systems, automating server configurations, and refining maintenance processes, resulting in a 50% reduction in ticket count and a 30% improvement in application uptime. My proactive approach in conducting root cause analyses and collaborating with stakeholders and developers led to the resolution of critical issues, earning recognition through the Insta Pat Award and enabling optimized performance and reliability across applications.",
+    skills: [
+      "Leadership",
+      "Root Cause Analysis",
+      "Python",
+      "AWS",
+      "Openstack",
+      "Bamboo CI/CD",
+      "Adobe Experence Manager (AEM)",
+      "Docker",
+      "ELK Stack",
+      "Bitbucket/GIT",
+      "Ansible",
+      "Java",
+      "Javascript",
+      "ReactJs",
+      "Nagios",
+      "Bash",
+      "Heat Template",
+      "ITIL",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQE4UC9Z_k3c7A/company-logo_200_200/0/1579422507409?e=1692230400&v=beta&t=HJssPsrwwkAJAsvFiVIYPdM8GzUuuSK5YlATKgLx3Bc",
+    school: "University of Texas at Arlington",
+    date: "Jan 2022 - Dec 2023",
+    degree: "Master in Science - Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://methodistschool-dankuni.webs.com/NEW%20BADGE.jpg",
+    school: "Savitribai Phule Pune University (Pune University)",
+    date: "Aug 2014 - July 2018",
+    degree: "Bachelor of Engineering - Computer Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Data Pipeline for User Behavior Analytics",
+    description:
+      "The professional designed and implemented a data pipeline with Apache Airflow, utilized Apache Spark for classifying movie reviews and integrated it with user purchase data, and leveraged Metabase for insightful data visualization of user behavior analytics, derived from a variety of sources including OLTP tables and external CSV files, to populate user-behavior-metric OLAP tables and ensure comprehensive analytics.",
+    image:
+      "https://github.com/thakur-rishabh/test/assets/73167868/9ff6f27a-5c11-4e38-bf8d-076af7f0b0c9",
+    tags: [
+      "AWS",
+      "Airflow",
+      "Python",
+      "AWS S3",
+      "AWS Redshift Spectrum",
+      "PostgreSQL",
+    ],
+  },
+  {
+    id: 0,
+    title: "Air quality Monitoring System",
+    description:
+      "I implemented both batch and streaming pipelines to facilitate the analysis of real-time and historical data, aiming to boost occupant productivity. By storing IoT sensor data in GCP Bigquery, I managed to optimize query speed significantly. Additionally, I developed a user-friendly dashboard using Dash and Plotly, which displays dynamic real-time graphs, making the interpreted data accessible and understandable to users, thereby allowing for more informed and timely decision-making processes.",
+    image: "https://github.com/thakur-rishabh/test/assets/73167868/9dc7fa22-ea0c-4265-b754-11eb2a54f0f2",
+    tags: ["IoT", "RaspberryPi", "SCD40", "Python", "GCP PubSub", "GCP Cloud function", "GCP Big Query", "GCP Cloud storage", "GCP Datadlow", "GCP Cloud Run"],
+    // category: "web app",
+    github: "https://github.com/thakur-rishabh/air-quality-monitoring-system",
+  },
+  {
+    id: 1,
+    title: "Phone-book API",
+    date: "Oct 2022 - Present",
+    description:
+      "I engineered a Phone-Book API, integrating it with SQLite to streamline the management and storage of user contact details. This involved developing a backend feature capable of processing user-input data and conducting validation to mitigate the risk of potential malicious content. The system is designed to generate output in a user-friendly JSON format, ensuring smooth and secure user interactions with the contact details stored in the database.",
+    image:
+      "https://github.com/thakur-rishabh/test/assets/73167868/e51ecdf4-c349-4ca0-a907-8ae33be5f757",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "NodeMailer",
+    ],
+    // category: "web app",
+    github: "https://github.com/rishavchanda/Project-Management-App",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
